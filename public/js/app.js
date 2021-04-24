8d7557c53d51d94f23f6f14ff0f411c709c4da6f
Pooja <poojaagnihotri29@gmail.com>
2021-04-25 01:35:43 +0530

    Remove Console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,12 @@
-console.log("client side javascript file loaded !")
+
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+
+
+
 
 weatherForm.addEventListener('submit',(e) =>{
     e.preventDefault()
